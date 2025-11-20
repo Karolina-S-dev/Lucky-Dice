9e8cee7cbfa48d3df77571c5c13392774055f7ac
Piotr Pepliński <p.peplaj@gmail.com>
2025-11-20 08:40:34 +0100

    added js for navbar on phones

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+let navBurger = document.querySelector('#nav-burger')
+let navLinks = document.querySelector('.nav-links')
+
+
+navBurger.addEventListener('click', (event)=>{
+    navLinks.style.opacity = 1
+})
+
